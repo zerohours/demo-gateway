557df4c7456ff793c69664603b08a4432b6f06e8
Julio Cesar Brizuela <brizuelaalvarado@gmail.com>
2023-01-29 14:29:41 -0600

    create a base start spring with spring-gateway

---
+++ src/test/java/com/brizuela/DemoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brizuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
